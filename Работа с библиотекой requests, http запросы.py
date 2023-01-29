f044b574f4f07e767fb6638ebf7ece22074ffff4
Lyota <korystin.aleksei@yandex.ru>
2023-01-29 17:35:25 +0300

    add file

--- Работа с библиотекой requests, http запросы.py
+++ Работа с библиотекой requests, http запросы.py
@@ -1,1 +1,22 @@
+import requests
+
 #Задача №1
+def identifying_smartest(name_heroes):
+    url = "https://akabab.github.io/superhero-api/api//all.json"
+    respons = requests.get(url=url)
+    list_heroes = name_heroes.split(', ')
+    heroes = {}
+    value_intelligence = []
+    for i in respons.json():
+        if i.get('name') in list_heroes:
+            heroes[i['name']] =  i['powerstats']['intelligence']
+    for number in heroes.values():
+        value_intelligence.append(int(number))
+    for key, value in heroes.items():
+        if value == max(value_intelligence):
+            res = f'Самый умный герой: {key} c количеством интеллекта  {value}'
+    return print(res)
+
+identifying_smartest('Captain America, Thanos, Hulk')
+
+#Задача №2
